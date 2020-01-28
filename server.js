dacebf9612e99b71ac84ce981f3de91b9f86ebcf
RihoSuurAju <rihomartin.haavasalu@gmail.com>
2020-01-28 09:56:23 +0200

    Shrek is my hero.

---
+++ server.js
@@ -0,0 +1,14 @@
+var superheroes = require('superheroes');
+var mySuperheroName = superheroes.random();
+console.log(mySuperheroName);
+
+// --------------------- //
+
+console.log();
+var supervillains = require('supervillains');
+var mySupervillainName = supervillains.random();
+console.log("All The Supervillain Names:");
+console.log(supervillains);
+console.log();
+console.log("Random Supervillain Name:")
+console.log(mySupervillainName);
